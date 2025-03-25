cf9d4f6657daf999c0e3352913091fef0e28fd27
jagger1407 <jackbroek10@gmail.com>
2025-03-25 20:07:05 +0100

    fixed memory management for array class

--- array.h
+++ array.h
@@ -19,11 +19,13 @@ public:
     Array<T>(u32 num_el) {
         _arr = NULL;
         while(_arr = (T*)malloc(num_el * sizeof(T)), _arr == NULL);
+        _cur = _arr;
         _count = num_el;
     }
     Array<T>(const Array<T>& arr) {
         _arr = NULL;
         while(_arr = (T*)malloc(arr._count * sizeof(T)), _arr == NULL);
+        _cur = _arr;
         _count = arr._count;
         for(int i=0;i<_count;i++) {
             _arr[i] = arr._arr[i];
@@ -32,16 +34,51 @@ public:
     ~Array<T>() {
         if(_arr != NULL) {
             free(_arr);
+            _arr = NULL;
+            _cur = NULL;
         }
     }
 
+    Array<T>& operator+(s32 add) {
+        Array<T> arr = Array<T>(*this);
+        arr += add;
+        return arr;
+    }
+    Array<T>& operator-(s32 sub) {
+        Array<T> arr = Array<T>(*this);
+        arr -= sub;
+        return arr;
+    }
+    Array<T>& operator++() {
+        _cur++;
+        return *this;
+    }
+    Array<T>& operator--() {
+        _cur--;
+        return *this;
+    }
+    Array<T>& operator +=(s32 increase) {
+        _cur += increase;
+        return *this;
+    }
+    Array<T>& operator -=(s32 decrease) {
+        _cur -= decrease;
+        return *this;
+    }
     T& operator[](u32 index) {
-        return _arr[index];
+        return _cur[index];
+    }
+    T& operator*() {
+        return *_cur;
+    }
+    T* ptr() {
+        return _cur;
     }
 
     u32 length() { return _count; }
 private:
     T* _arr;
+    T* _cur;
     u32 _count;
 };
 
