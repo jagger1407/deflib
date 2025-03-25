e5ca09bfd74a930a41b03a93f4d26f5d16fc39ff
jagger1407 <jackbroek10@gmail.com>
2025-03-25 16:08:23 +0100

    Added string class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include "string.h"
+#include <stdio.h>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+int Main(int argc, string* args) {
+    puts(args[0].c_str());
+
     return 0;
 }
+
+#if 1 // call main
+int main(int argc, char **argv) {
+    string args[argc-1];
+
+    for(int i=1;i<argc;i++) {
+        args[i-1] = argv[i];
+    }
+
+    return Main(argc, args);
+}
+#endif
