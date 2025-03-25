065e271bc14702458cf8c35c8e239ce9212aaf02
jagger1407 <jackbroek10@gmail.com>
2025-03-25 21:42:19 +0100

    Changed internal var name, slight doc
    doc stands for documentation chat
    I bet you couldn't do 5 * 1000 without pulling out the docs
    you're not impressing my stream

--- array.h
+++ array.h
@@ -6,35 +6,35 @@
 
 #define TYPE template<typename T>
 
-/**
- * @todo write docs
+/** Array class
+ * Wrapper for any kind of array, but with memory safety and all that
  */
 TYPE
 class Array
 {
 public:
     Array<T>() {
-        _arr = NULL;
+        _arrptr = NULL;
     }
     Array<T>(u32 num_el) {
-        _arr = NULL;
-        while(_arr = (T*)malloc(num_el * sizeof(T)), _arr == NULL);
-        _cur = _arr;
+        _arrptr = NULL;
+        while(_arrptr = (T*)malloc(num_el * sizeof(T)), _arrptr == NULL);
+        _cur = _arrptr;
         _count = num_el;
     }
     Array<T>(const Array<T>& arr) {
-        _arr = NULL;
-        while(_arr = (T*)malloc(arr._count * sizeof(T)), _arr == NULL);
-        _cur = _arr;
+        _arrptr = NULL;
+        while(_arrptr = (T*)malloc(arr._count * sizeof(T)), _arrptr == NULL);
+        _cur = _arrptr;
         _count = arr._count;
         for(int i=0;i<_count;i++) {
-            _arr[i] = arr._arr[i];
+            _arrptr[i] = arr._arrptr[i];
         }
     }
     ~Array<T>() {
-        if(_arr != NULL) {
-            free(_arr);
-            _arr = NULL;
+        if(_arrptr != NULL) {
+            free(_arrptr);
+            _arrptr = NULL;
             _cur = NULL;
         }
     }
@@ -77,7 +77,7 @@ public:
 
     u32 length() { return _count; }
 private:
-    T* _arr;
+    T* _arrptr;
     T* _cur;
     u32 _count;
 };
