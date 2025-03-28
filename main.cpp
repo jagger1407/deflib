bb65783a4f0ec3ca4919edb82d49b365457b2f7e
jagger1407 <jagger.pib@gmail.com>
2025-03-28 21:16:05 +0100

    added subarray method

--- main.cpp
+++ main.cpp
@@ -2,22 +2,14 @@
 #include "deflib/deflib.h"
 
 void Program::Main(StringArray args) {
+    Int32Array ia = { 1, 2, 3, 4, 5 };
 
-    File f(args[0], File::OpenMode::ReadWrite, false);
-    if(!f.isOpen()) {
-        DefCon::PrintLine("Opening file failed!");
-        return;
+    Int32Array subarray = ia.subarray(2, 2);
+
+    for(int i = 0; i < subarray.count(); i++) {
+        DefCon::Print(Cvt::ToString(subarray[i]) + " ");
     }
-
-    f.seek(0, SEEK_END);
-    f.writeLine("\nTesting");
-    f.seek(0, SEEK_SET);
-
-    StringArray lines = Cvt::ToString(f.readFile()).split('\n');
-
-    for(int i = 0; i < lines.count(); i++) {
-        DefCon::PrintLine(Cvt::ToString(i+1) + ": " + lines[i]);
-    }
+    DefCon::PrintLine("");
 }
 
 
