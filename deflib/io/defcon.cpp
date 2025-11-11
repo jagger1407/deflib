44bb885d3d3565d6d64cd5aab88528a36f2145a2
jagger1407 <jagger.pib@gmail.com>
2025-11-11 14:07:38 +0100

    DefCon: Add PrintLine & Print funcs for basic data types

--- deflib/io/defcon.cpp
+++ deflib/io/defcon.cpp
@@ -3,11 +3,87 @@
 void DefCon::PrintLine() {
     puts("");
 }
+void DefCon::PrintLine(const u8 c) {
+    printf("%c\n", c);
+}
+void DefCon::PrintLine(const char c) {
+    printf("%c\n", c);
+}
+void DefCon::PrintLine(const s16 val) {
+    printf("%d\n", val);
+}
+void DefCon::PrintLine(const u16 val) {
+    printf("%u\n", val);
+}
+void DefCon::PrintLine(const s32 val) {
+    printf("%d\n", val);
+}
+void DefCon::PrintLine(const u32 val) {
+    printf("%u\n", val);
+}
+void DefCon::PrintLine(const s64 val) {
+    printf("%lld\n", val);
+}
+void DefCon::PrintLine(const u64 val) {
+    printf("%llu\n", val);
+}
+void DefCon::PrintLine(const float val) {
+    printf("%f\n", val);
+}
+void DefCon::PrintLine(const double val) {
+    printf("%f\n", val);
+}
 void DefCon::PrintLine(const string& str) {
     puts(str.c_str());
 }
+void DefCon::PrintLine(const Array<char>& arr) {
+    puts(string((Array<char>)arr).c_str());
+}
+void DefCon::PrintLine(const Array<u8>& arr) {
+    puts(string((Array<u8>)arr).c_str());
+}
+
+void DefCon::Print(const u8 c) {
+    printf("%c", c);
+}
+void DefCon::Print(const char c) {
+    printf("%c", c);
+}
+void DefCon::Print(const s16 val) {
+    printf("%d", val);
+}
+void DefCon::Print(const u16 val) {
+    printf("%u", val);
+}
+void DefCon::Print(const s32 val) {
+    printf("%d", val);
+}
+void DefCon::Print(const u32 val) {
+    printf("%u", val);
+}
+void DefCon::Print(const s64 val) {
+    printf("%lld", val);
+}
+void DefCon::Print(const u64 val) {
+    printf("%llu", val);
+}
+void DefCon::Print(const float val) {
+    printf("%f", val);
+}
+void DefCon::Print(const double val) {
+    printf("%f", val);
+}
 void DefCon::Print(const string& str) {
     printf("%s", str.c_str());
+}
+void DefCon::Print(const Array<char>& arr) {
+    printf("%s", string((Array<char>)arr).c_str());
+}
+void DefCon::Print(const Array<u8>& arr) {
+    printf("%s", string((Array<u8>)arr).c_str());
+}
+void DefCon::Print(const char* ptr) {
+    printf("%s", ptr);
 }
 
 void DefCon::Printf(const string& format, ...) {
--- deflib/io/defcon.h
+++ deflib/io/defcon.h
@@ -17,13 +17,117 @@ public:
      */
     static void PrintLine();
     /**
+     * Prints character c to the stdout console and enters a new line.
+     */
+    static void PrintLine(const u8 c);
+    /**
+     * Prints character c to the stdout console and enters a new line.
+     */
+    static void PrintLine(const char c);
+    /**
+     * Prints value val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const s16 val);
+    /**
+     * Prints value val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const u16 val);
+    /**
+     * Prints value val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const s32 val);
+    /**
+     * Prints value val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const u32 val);
+    /**
+     * Prints value val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const s64 val);
+    /**
+     * Prints value val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const u64 val);
+    /**
+     * Prints value val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const float val);
+    /**
+     * Prints value val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const double val);
+    /**
      * Prints str to the stdout console and enters a new line.
      */
     static void PrintLine(const string& str);
     /**
-     * Prints str to the stdout console without line-breaking.
+     * Prints char array val to the stdout console and enters a new line.
+     */
+    static void PrintLine(const Array<char>& arr);
+    /**
+     * Prints char array arr to the stdout console and enters a new line.
+     */
+    static void PrintLine(const Array<u8>& arr);
+    /**
+     * Prints string pointed to by ptr to the stdout console and enters a new line.
+     */
+    static void PrintLine(const char* ptr);
+    /**
+     * Prints character c to the stdout console.
+     */
+    static void Print(const u8 c);
+    /**
+     * Prints character c to the stdout console.
+     */
+    static void Print(const char c);
+    /**
+     * Prints value val to the stdout console.
+     */
+    static void Print(const s16 val);
+    /**
+     * Prints value val to the stdout console.
+     */
+    static void Print(const u16 val);
+    /**
+     * Prints value val to the stdout console.
+     */
+    static void Print(const s32 val);
+    /**
+     * Prints value val to the stdout console.
+     */
+    static void Print(const u32 val);
+    /**
+     * Prints value val to the stdout console.
+     */
+    static void Print(const s64 val);
+    /**
+     * Prints value val to the stdout console.
+     */
+    static void Print(const u64 val);
+    /**
+     * Prints value val to the stdout console.
+     */
+    static void Print(const float val);
+    /**
+     * Prints value val to the stdout console.
+     */
+    static void Print(const double val);
+    /**
+     * Prints str to the stdout console.
      */
     static void Print(const string& str);
+    /**
+     * Prints char array val to the stdout console.
+     */
+    static void Print(const Array<char>& arr);
+    /**
+     * Prints char array arr to the stdout console.
+     */
+    static void Print(const Array<u8>& arr);
+    /**
+     * Prints string pointed to by ptr to the stdout console.
+     */
+    static void Print(const char* ptr);
     /**
      * Prints a formatted string to stdout.
      * Essentially a wrapper for the C printf() function.
